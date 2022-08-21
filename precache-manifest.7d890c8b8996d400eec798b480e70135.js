d659ff3bb92cc8495233a2d3b465315fa954166b
mdammar786 <98681797+mdammar786@users.noreply.github.com>
2022-08-22 02:20:40 +0530

    Updates

---
+++ precache-manifest.7d890c8b8996d400eec798b480e70135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102221456526198c4937bef6b0cac103",
+    "url": "/mdammar786/ecommerce/index.html"
+  },
+  {
+    "revision": "94ca4dd29c0a881baa41",
+    "url": "/mdammar786/ecommerce/static/js/2.ba93ba03.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/mdammar786/ecommerce/static/js/2.ba93ba03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b42a3f95bf1e68a529",
+    "url": "/mdammar786/ecommerce/static/js/main.1fa3bf87.chunk.js"
+  },
+  {
+    "revision": "2faff2828178681a6294",
+    "url": "/mdammar786/ecommerce/static/js/runtime-main.942fcf7c.js"
+  },
+  {
+    "revision": "3bf24e4b8d8a2563872313e085751b5f",
+    "url": "/mdammar786/ecommerce/static/media/commerce.3bf24e4b.png"
+  }
+]);
